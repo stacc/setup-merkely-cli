7a258dd2c4ae20ba7529dddd9cc29da3f7b595f5
Øyvind Fanebust <oyvind.fanebust@gmail.com>
2022-06-08 09:27:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const core = require("@actions/core");
+const tc = require("@actions/tool-cache");
+const { getDownloadUrl } = require("./download");
+
+async function setup() {
+  try {
+    const version = core.getInput("version");
+    const platform = os.platform();
+    const arch = os.arch();
+    const downloadUrl = getDownloadUrl({ version, platform, arch });
+
+    const pathToTarball = await tc.downloadTool(downloadUrl);
+
+    const pathToCLI = await tc.extractTar(pathToTarball);
+    core.addPath(pathToCLI);
+  } catch (e) {
+    core.setFailed(e);
+  }
+}
+
+module.exports = setup;
+
+if (require.main === module) {
+  setup();
+}
